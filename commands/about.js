a626e001a67b569db70709e84a02f37f2d5d2c3a
Magicperson1 <37675257+Magicperson1@users.noreply.github.com>
2018-07-29 16:48:32 -0500

    fixed major security issue
    added new features as well

--- commands/about.js
+++ commands/about.js
@@ -1,9 +1,17 @@
 module.exports = {
-    name: 'about-me',
+    name: 'about',
 	cooldown: 2,
     description: 'shows user information',
     execute(message, args) {
+	if (!args.length) {	
         message.channel.send('Username: ' + message.author.username + '\nYour ID: ' + message.author.id + '\nYour Avatar:')
 		message.channel.send(message.author.avatarURL)
-    },
+    } 
+	else {
+		message.channel.send('Username: ' + message.mentions.users.first().username + '\nID: ' + message.mentions.users.first().id + '\nAvatar:')
+		message.channel.send(message.mentions.users.first().avatarURL)
+	}
+	
+	},
+	
 };
