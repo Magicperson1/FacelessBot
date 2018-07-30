39255229d5e055a439ec03b32c7ff6125aecfca2
Magicperson1 <37675257+Magicperson1@users.noreply.github.com>
2018-07-30 18:29:12 -0500

    1.1.1
    Fixed kick command and changed prefix

--- commands/kick.js
+++ commands/kick.js
@@ -8,10 +8,18 @@ module.exports = {
     return message.reply('you need to tag a user in order to kick them!');
     }// grab the "first" mentioned user from the message
     // this will return a `User` object, just like `message.author`
-    var member = message.mentions.users.first();
+    var member = message.mentions.users.first()
+
     if (message.member.permissions.has('KICK_MEMBERS')) {
-        message.mentions.kick();      
+		console.log(member)
+        message.guild.member(member).kick();    
+		message.channel.send(`**You have kicked:** ${member}`)
+		const ChannelLog = member.guild.channels.find(ch => ch.name === 'member-log');
+        if (!ChannelLog) return;
+		ChannelLog.send(`${member} has been kicked by ${message.author}`)
 	}	
-    message.channel.send(`You have kicked: ${member.username}`);
+	else {
+		message.channel.send(`${message.author} **You do not have the proper permissions!**`)
+	}
     }
 };
